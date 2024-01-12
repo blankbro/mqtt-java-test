9569547a1267594ede69d30f6b4ad1f150a2f5ff
lizexin <zexin-li@outlook.com>
2024-01-12 15:52:21 +0800

    first commit

---
+++ src/main/java/io/github/blankbro/mqttjavatest/MqttJavaTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.blankbro.mqttjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttJavaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/blankbro/mqttjavatest/MqttJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.blankbro.mqttjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
